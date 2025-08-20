3419640200dccf525533f2421709dd6ab1808005
sang-tx <118146176+sang-tx@users.noreply.github.com>
2025-08-20 14:19:20 +0700

    Create AboutUs.tsx

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,23 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import CTA from "@/components/CTA";
+import Navigation from "@/components/Navigation";
+
+const AboutUs = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <div id="home">
+        <Hero />
+      </div>
+      <div id="features">
+        <Features />
+      </div>
+      <div id="about">
+        <CTA />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
